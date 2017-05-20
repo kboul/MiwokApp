84c477792c752c71a1760193a706698d45c2c70a
kboul <kos.voulgaridis@gmail.com>
2017-05-20 19:58:45 +0300

    Add a second constructor which will have 3 input parameters

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,5 +1,7 @@
 package com.example.android.miwok;
 
+import java.lang.reflect.Constructor;
+
 /**
  * {@link Word} represents a vocabulary word that the user wants to learn.
  * It contains a default translation and a Miwok translation for that word.
@@ -16,10 +18,26 @@ public class Word {
     //Store image resource id to associate icon with word
     private int mImageResourceId;
 
-    //Constructor which takes in two string parameters
+    /**
+     * Constructor which takes in two string parameters
+     * @param defaultTranslation is the word in a English language
+     * @param miwokTranslation is the word in Miwok language
+     */
     public Word(String defaultTranslation, String miwokTranslation) {
         mDeafaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Constructor which takes in two string parameters and one image resource id
+     * @param defaultTranslation is the word in a English language
+     * @param miwokTranslation is the word in Miwok language
+     * @param imageResourceID is the drawable resource id of the image associated with the word
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID) {
+        mDeafaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceID;
     }
 
     /**
